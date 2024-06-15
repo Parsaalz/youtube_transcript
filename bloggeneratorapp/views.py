447d3f5d627f31edfc904a02122c9886253ec579
Parsa <parsaalizadeh2022@gmail.com>
2024-06-15 15:10:25 +0430

    complete

--- bloggeneratorapp/views.py
+++ bloggeneratorapp/views.py
@@ -6,41 +6,64 @@ from pytube import YouTube
 from django.conf import settings
 import os
 import assemblyai as aai
-def blog_page(request):
-    return render(request,'base.html')
+import openai as oai
 @csrf_exempt
-def generate_blog(request):
+def blog_page(request):
     if request.method=="POST":
-        try:
-            data=json.loads(request.body)
-            yt_link=data["link"]
-            return JsonResponse({"content":yt_link})
-        except(KeyError,json.JSONDecodeError):
-            return JsonResponse({"error":"invalid data sent"},status=400)
+        yt_link=request.POST.get('youtubelink')
+        print(yt_link)
         title=get_title(yt_link)
         transcription=get_transcription(yt_link)
         if not transcription:
             return JsonResponse({"error":"failed to get transcript"},status=500)
-    else:
-        return JsonResponse({"eror":"invalid request method"},status=405)
+        blog_content=generate_blog(transcription)
+        print(blog_content)
+        if not blog_content:
+            return JsonResponse({"error":"failed to generate"},status=500)
+    return render(request,'base.html')
+
 
 def get_title(link):
+    print(1)
+    print(link)
     yt=YouTube(link)
     title=yt.title
+    print(title)
     return title
 
 def get_audio(link):
+    print(2)
     yt=YouTube(link)
+    print(2.1)
     video=yt.streams.filter(only_audio=True).first()
+    print(2.2)
     audio=video.download(output_path=settings.MEDIA_ROOT)
+    print(2.3)
     base,ext=os.path.splitext(audio)
+    print(2.4)
     new_file=base+'.mp3'
+    print(2.5)
     os.rename(audio,new_file)
     return new_file
 
 def get_transcription(link):
+    print(3)
     audio_file=get_audio(link)
-    aai.settings.api_key = "06b8d0f97d84421b81fde120af29d6bd"
+    # aai.settings.api_key = ""
     transcriber=aai.transcriber()
     transcript=transcriber.transcribe(audio_file)
     return transcript.text
+
+def generate_blog(transcription):
+    print(4)
+    # oai.api_key=""
+
+    prompt=f"{transcription}\n\nArticle:"
+
+    response = oai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        max_tokens=1000,
+    )
+    generated_content=response.choices[0].text.strip()
+    return generated_content
